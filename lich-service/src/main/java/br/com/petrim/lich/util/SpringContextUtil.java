8f411fb65c9c8fe35fc67bb5ff291388fa7eaaac
Welson Carvalho <welsonfscarvalho@gmail.com>
2019-02-02 12:47:39 -0200

    correções sonar

--- lich-service/src/main/java/br/com/petrim/lich/util/SpringContextUtil.java
+++ lich-service/src/main/java/br/com/petrim/lich/util/SpringContextUtil.java
@@ -1,6 +1,7 @@
 package br.com.petrim.lich.util;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringContextUtil implements ApplicationContextAware {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    @Autowired
+    public SpringContextUtil(ApplicationContext applicationContext) {
         ApplicationContextUtil.context = applicationContext;
     }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {}
 
     public static Object getBean(String beanName) {
         return ApplicationContextUtil.context.getBean(beanName);
